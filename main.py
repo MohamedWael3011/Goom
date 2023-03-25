641bc3db2d150a172fdd71a0a946983b13f7f7f4
Mohamed Wael <Mohamedwael57309@gmail.com>
2023-03-26 01:56:12 +0200

    Typo

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import json
 client = commands.Bot(command_prefix='.', intents=intents)
 load_dotenv()
 MemeList = ["hand", "gunball", "stonks"]
-Legendaries = ["rarest goombles of all time", "kespin cone", "powdered peaks", "magic sugar forest", "cavity court", "caramal swamps"]
+Legendaries = ["rarest goombles of all time", "kespin cone", "powdered peaks", "magic sugar forest", "cavity court", "caramel swamp"]
 def AllMeme():
   s=""
   for i in MemeList:
