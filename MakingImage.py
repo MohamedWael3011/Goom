2ee5bb6014f3b36efbfa6308c13423f479f268fb
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2023-05-27 21:17:03 +0300

    test

--- MakingImage.py
+++ MakingImage.py
@@ -46,13 +46,14 @@ def GenerateImage(GoombleID,Traits,meme):  #Will need to add a new argument for 
             AttributeName = att[1:]  #Removing the sort number
             if Traits[AttributeName] != 'none':
                 AttributePath = os.path.join(layer,Traits[AttributeName]+".png")
+                print(AttributePath)
                 RenderingTraits.append(AttributePath)
 
     TransparentImage = Image.new("RGBA", (4096,4096))
 
     for layer in RenderingTraits:  #Without Background
       LayerImage = Image.open(layer)
-      if meme =="hand" and Traits["flavor"] +".png" == layer:
+      if meme =="hand" and Traits["flavor"] in layer:
         if Traits["body"] =="round":
           img_mask = Image.open('maskR.png')
           img_mask = img_mask.convert('L')
